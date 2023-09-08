53a892651a627d6762325ea994506908e048f9eb
John <john@zklink.org>
2023-09-09 00:45:28 +0800

    update custom chain rpc

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -5,8 +5,9 @@ export const ZKLINK_RPC_ENDPOINT = process.env.ZKLINK_RPC_ENDPOINT;
 export const ZKLINK_RPC_ENDPOINT_PORT = process.env.ZKLINK_RPC_ENDPOINT_PORT;
 
 export const custormChainRPC: { [chainId: number]: string | undefined } = {
-  10: `https://1rpc.io/matic`,
+  137: `https://polygon-mainnet.infura.io/v3/${process.env.INFURE_KEY}`,
   7: `https://linea-mainnet.infura.io/v3/${process.env.INFURE_KEY}`,
   2: `https://avalanche-mainnet.infura.io/v3/${process.env.INFURE_KEY}`,
   4: `https://mainnet.infura.io/v3/${process.env.INFURE_KEY}`,
+  10: "https://optimism.publicnode.com",
 };
